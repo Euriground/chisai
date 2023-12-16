84e1dac6b7471e87421792383caca1a99110419d
mr-amirfazel <amirfazel45@gmail.com>
2023-12-16 18:01:14 +0330

    initialized db

---
+++ db/atlas.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+    "context"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "log"
+    "time"
+)
+
+var client *mongo.Client
+
+func ConnectDB() error {
+    clientOptions := options.Client().ApplyURI("mongodb+srv://amirfazel45:<123>@sample-cluster.087lnjs.mongodb.net/?retryWrites=true&w=majority")
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    var err error
+    client, err = mongo.Connect(ctx, clientOptions)
+    if err != nil {
+        log.Fatal(err)
+        return err
+    }
+
+    // Check the connection
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        log.Fatal(err)
+        return err
+    }
+
+    return nil
+}
+
+func GetDBInstance() *mongo.Client {
+    return client
+}
